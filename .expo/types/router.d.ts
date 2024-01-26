15e5a4d991cc2abc43f04a8edfb8c3f72ce8983c
Eadweard <804875728@qq.com>
2024-01-26 17:10:14 +0800

    fix

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -7,7 +7,7 @@ declare module "expo-router" {
   export * from 'expo-router/build';
 
   // prettier-ignore
-  type StaticRoutes = `/` | `/(tabs)/` | `/(tabs)` | `/(tabs)/two` | `/two` | `/.DS_Store` | `/modal` | `/home`;
+  type StaticRoutes = `/` | `/(tabs)/` | `/(tabs)` | `/(tabs)/two` | `/two` | `/.DS_Store` | `/home` | `/modal`;
   // prettier-ignore
   type DynamicRoutes<T extends string> = never;
   // prettier-ignore
